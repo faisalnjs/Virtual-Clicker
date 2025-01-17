707a54edec123b3fed4649f98f4a6f8c4175f446
Faisal N <contact@faisaln.com>
2025-01-17 10:47:43 -0500

    Fix date time loss bug
    Signed-off-by: Faisal N <contact@faisaln.com>

--- src/clicker/clicker.js
+++ src/clicker/clicker.js
@@ -327,7 +327,7 @@ document.getElementById("history-last").addEventListener("click", () => {
 // Count number of unique days
 function getHistoryDates() {
   const data = (storage.get("history") || []).map((entry) => {
-    const day = new Date(entry.timestamp).toISOString().split("T")[0];
+    const day = entry.timestamp;
     return { ...entry, day: day };
   });
   const unique = data
@@ -342,7 +342,7 @@ function getHistoryDates() {
 // Filter history by date
 function filterHistory() {
   const data = (storage.get("history") || []).map((entry) => {
-    const day = new Date(entry.timestamp).toISOString().split("T")[0];
+    const day = entry.timestamp;
     return { ...entry, day: day };
   });
   return data.filter((entry) => entry.day === getHistoryDates()[historyIndex]);
@@ -358,7 +358,7 @@ function updateHistory() {
       year: "numeric",
       month: "long",
       day: "numeric",
-    }).format(new Date(history[0]?.day));
+    }).format(history[0]?.day);
 
   // Update history navigation
   document.getElementById("history-first").disabled = historyIndex === getHistoryDates().length - 1;
