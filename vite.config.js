93d24309f84b2d8a9e596e6e0f5c96b2b73fffd8
Faisal N <contact@faisaln.com>
2025-02-06 10:49:15 -0500

    Fixes

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import webfontDownload from "vite-plugin-webfont-dl";
 import version from "vite-plugin-package-version";
-const packageJson = require('./package.json');
-const version = packageJson.version;
+const package = require('./package.json');
 
 export default {
   plugins: [webfontDownload(), version()],
   build: {
     rollupOptions: {
       output: {
-          entryFileNames: `[name]-[hash].[format]?v=${version().version}`,
-          chunkFileNames: `[name]-[hash].[format]?v=${version().version}`,
-          assetFileNames: `[name]-[hash].[ext]?v=${version().version}`
+          entryFileNames: `[name]-[hash].[format]?v=${package.version}`,
+          chunkFileNames: `[name]-[hash].[format]?v=${package.version}`,
+          assetFileNames: `[name]-[hash].[ext]?v=${package.version}`
       }
     }
   }
