f4a6ddfeff6084373f897af1bbf3ac82f2229d04
Faisal N <contact@faisaln.com>
2025-02-06 11:00:50 -0500

    Use defineConfig

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import webfontDownload from "vite-plugin-webfont-dl";
-import version from "vite-plugin-package-version";
-const packageJSON = require('./package.json');
+import { defineConfig } from 'vite';
+import webfontDownload from 'vite-plugin-webfont-dl';
+import version from 'vite-plugin-package-version';
+import packageJSON from './package.json';
 
-export default {
+export default defineConfig({
   plugins: [webfontDownload(), version()],
   build: {
     rollupOptions: {
       output: {
-          entryFileNames: `[name]-[hash].js?v=${packageJSON.version}`,
-          chunkFileNames: `[name]-[hash].js?v=${packageJSON.version}`,
-          assetFileNames: `[name]-[hash].[ext]?v=${packageJSON.version}`
+        entryFileNames: `[name]-[hash].js?v=${packageJSON.version}`,
+        chunkFileNames: `[name]-[hash].js?v=${packageJSON.version}`,
+        assetFileNames: `[name]-[hash].[ext]?v=${packageJSON.version}`
       }
     }
   }
-};
+});
