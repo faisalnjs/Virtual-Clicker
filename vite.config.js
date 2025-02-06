ffbae4e1c108483a2de0402c12407d9e551127ad
Faisal N <contact@faisaln.com>
2025-02-06 11:08:17 -0500

    Restore
    Signed-off-by: Faisal N <contact@faisaln.com>

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
-import { defineConfig } from 'vite';
 import webfontDownload from 'vite-plugin-webfont-dl';
 import version from 'vite-plugin-package-version';
-import packageJSON from './package.json';
 
 export default defineConfig({
-  plugins: [webfontDownload(), version()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name]-[hash].js?v=${packageJSON.version}`,
-        chunkFileNames: `[name]-[hash].js?v=${packageJSON.version}`,
-        assetFileNames: `[name]-[hash].[ext]?v=${packageJSON.version}`
-      }
-    }
-  }
+  plugins: [webfontDownload(), version()]
 });
