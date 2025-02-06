3b44a393957311904ebcd33b586b3bf44c3f4468
Faisal N <contact@faisaln.com>
2025-02-06 10:44:10 -0500

    Fix cache busting

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import webfontDownload from "vite-plugin-webfont-dl";
 import version from "vite-plugin-package-version";
+const packageJson = require('./package.json');
+const version = packageJson.version;
 
 export default {
   plugins: [webfontDownload(), version()],
   build: {
     rollupOptions: {
       output: {
-          entryFileNames: `[name].js?v=${version().version}`,
-          chunkFileNames: `[name].js?v=${version().version}`,
-          assetFileNames: `[name].[ext]?v=${version().version}`
+          entryFileNames: `[name]-[hash].[format]?v=${version().version}`,
+          chunkFileNames: `[name]-[hash].[format]?v=${version().version}`,
+          assetFileNames: `[name]-[hash].[ext]?v=${version().version}`
       }
     }
   }
