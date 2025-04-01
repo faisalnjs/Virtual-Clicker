58dcdc01d0275d2dd85da49be07598c5d4721773
Faisal N <contact@faisaln.com>
2025-04-01 14:12:58 -0400

    Fix storage.js

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,76 +1,71 @@
-try {
-  class Storage {
-    constructor(id) {
-      this.id = id;
-      this.syncWithCookie();
-    }
-
-    set(key, value) {
-      let temp = this.object;
-      temp[key] = value;
-      localStorage.setItem(this.id, JSON.stringify(temp));
-      setCookie(this.id, JSON.stringify(temp), 7);
-      return this;
-    }
-
-    get(key) {
-      return this.object[key];
-    }
-
-    all() {
-      return this.object;
-    }
-
-    delete(key) {
-      let temp = this.object;
-      delete temp[key];
-      localStorage.setItem(this.id, JSON.stringify(temp));
-      setCookie(this.id, JSON.stringify(temp), 7);
-      return this;
-    }
-
-    obliterate() {
-      localStorage.removeItem(this.id);
-      setCookie(this.id, "", -1);
-    }
-
-    get object() {
-      return JSON.parse(localStorage.getItem(this.id)) || {};
-    }
-
-    syncWithCookie() {
-      const cookieData = getCookie(this.id);
-      if (cookieData) {
-        localStorage.setItem(this.id, cookieData);
-      }
-    }
+class Storage {
+  constructor(id) {
+    this.id = id;
+    this.syncWithCookie();
   }
 
-  function setCookie(name, value, days) {
-    let expires = "";
-    if (days) {
-      const date = new Date();
-      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-      expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = `${name}=${(value || "")}${expires}; path=/; domain=.${document.domain}`;
+  set(key, value) {
+    let temp = this.object;
+    temp[key] = value;
+    localStorage.setItem(this.id, JSON.stringify(temp));
+    setCookie(this.id, JSON.stringify(temp), 7);
+    return this;
   }
 
-  function getCookie(name) {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
+  get(key) {
+    return this.object[key];
   }
 
-  export default new Storage("virtual-falcons");
-} catch (error) {
-  if (storage.get("developer")) {
-    alert('Error @ storage.js:', error.message);
-  };
-  throw error;
-};
+  all() {
+    return this.object;
+  }
+
+  delete(key) {
+    let temp = this.object;
+    delete temp[key];
+    localStorage.setItem(this.id, JSON.stringify(temp));
+    setCookie(this.id, JSON.stringify(temp), 7);
+    return this;
+  }
+
+  obliterate() {
+    localStorage.removeItem(this.id);
+    setCookie(this.id, "", -1);
+  }
+
+  get object() {
+    return JSON.parse(localStorage.getItem(this.id)) || {};
+  }
+
+  syncWithCookie() {
+    const cookieData = getCookie(this.id);
+    if (cookieData) {
+      localStorage.setItem(this.id, cookieData);
+    }
+  }
+}
+
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = `${name}=${(value || "")}${expires}; path=/; domain=.${document.domain}`;
+}
+
+function getCookie(name) {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+const storageInstance = new Storage("virtual-falcons");
+
+export default storageInstance;
