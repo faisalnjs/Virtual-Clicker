ddfbf5847a65542be90acca48b01073b8c2ff40e
Faisal N <contact@faisaln.com>
2025-04-01 11:17:53 -0400

    Catch developer errors

--- src/periods/periods.js
+++ src/periods/periods.js
@@ -1,34 +1,41 @@
-import schedule from "./schedule.json";
-import extendedSchedule from "./extendedSchedule.json";
+try {
+  import schedule from "./schedule.json";
+  import extendedSchedule from "./extendedSchedule.json";
 
-const timestamps = schedule.map((period) => {
-  return [timeToMs(period[0]), timeToMs(period[1])];
-});
-
-const extendedTimestamps = extendedSchedule.map((period) => {
-  return [timeToMs(period[0]), timeToMs(period[1])];
-});
-
-// Returns zero-indexed period
-export function getPeriod(date) {
-  date = date || Date.now();
-  const period = timestamps.findIndex((periods) => {
-    return date >= periods[0] && date < periods[1];
+  const timestamps = schedule.map((period) => {
+    return [timeToMs(period[0]), timeToMs(period[1])];
   });
-  return period;
-}
 
-export function getExtendedPeriod(date) {
-  date = date || Date.now();
-  const period = extendedTimestamps.findIndex((periods) => {
-    return date >= periods[0] && date < periods[1];
+  const extendedTimestamps = extendedSchedule.map((period) => {
+    return [timeToMs(period[0]), timeToMs(period[1])];
   });
-  return period;
-}
 
-// Converts hh:mm to milliseconds
-export function timeToMs(time) {
-  const now = new Date();
-  const [hours, minutes] = time.split(":").map(Number);
-  return new Date(now.getFullYear(), now.getMonth(), now.getDate(), hours, minutes).getTime();
-}
+  // Returns zero-indexed period
+  export function getPeriod(date) {
+    date = date || Date.now();
+    const period = timestamps.findIndex((periods) => {
+      return date >= periods[0] && date < periods[1];
+    });
+    return period;
+  }
+
+  export function getExtendedPeriod(date) {
+    date = date || Date.now();
+    const period = extendedTimestamps.findIndex((periods) => {
+      return date >= periods[0] && date < periods[1];
+    });
+    return period;
+  }
+
+  // Converts hh:mm to milliseconds
+  export function timeToMs(time) {
+    const now = new Date();
+    const [hours, minutes] = time.split(":").map(Number);
+    return new Date(now.getFullYear(), now.getMonth(), now.getDate(), hours, minutes).getTime();
+  }
+} catch (error) {
+  if (storage.get("developer")) {
+    alert('Error @ periods.js:', error.message);
+  };
+  throw error;
+};
