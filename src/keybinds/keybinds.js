0c846659780194ceb6a0c8e6ffa741af12b7fdea
Faisal N <contact@faisaln.com>
2026-01-04 13:13:54 -0500

    Ctrl+S keybind launches make suggestion modal Fixes #83

--- src/keybinds/keybinds.js
+++ src/keybinds/keybinds.js
@@ -9,17 +9,16 @@ try {
       (dialog) => dialog.open,
     );
     const isTyping = document.activeElement.matches("input, textarea");
+    e.preventDefault();
     if (e.ctrlKey) {
       if (e.key == "Enter" && !anyDialogOpen) document.getElementById("submit-button").click();
       if (e.key == "," && !anyDialogOpen) ui.view("settings");
       if (e.key == "." && !anyDialogOpen) ui.view("history");
       if (e.key == "/" && !anyDialogOpen) ui.view("settings/keybinds");
       if (e.key == "b") ui.reportBugModal();
+      if (e.key == "s" && !anyDialogOpen) ui.suggestionsModal();
     } else if (e.altKey) {
-      if (/[1-9]/.test(e.key)) {
-        e.preventDefault();
-        insertFromIndex(parseInt(e.key) - 1);
-      }
+      if (/[1-9]/.test(e.key)) insertFromIndex(parseInt(e.key) - 1);
     } else if (e.shiftKey) {
       if (e.key == "R" && !anyDialogOpen && !isTyping) {
         themes.resetTheme();
